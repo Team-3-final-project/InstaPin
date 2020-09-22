e9aeaed704e021875d02d927e70f3da842aef3f1
Adek Ferdian <adekferdian@gmail.com>
2020-09-22 23:45:48 +0700

    fix conflict

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { userReducer } from './reducers/userReducer'
 import { profileReducers } from './reducers';
-import thunk from 'redux-thunk';
 
 const reducers = combineReducers({
   profileReducers,
