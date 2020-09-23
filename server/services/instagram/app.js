c6f5898a64960c9c09c53529d7491a7055156bd4
bobkombat <rdhwnrmdhny@gmail.com>
2020-09-23 10:16:57 +0700

    none

--- server/services/instagram/app.js
+++ server/services/instagram/app.js
@@ -11,4 +11,4 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-app.listen(PORT, () => console.log("at port 3001"));
+app.listen(PORT, () => console.log(`at port ${PORT}`));
