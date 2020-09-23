696455dc5fda0a9dd39bf26d5ef2714443a497ba
grine-mint <gracecorine60@gmail.com>
2020-09-23 11:23:55 +0700

    update detail in favorites for testing purposes

---
+++ server/services/favorites/helpers/checkUser.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+
+module.exports = async (email) => {
+    try{
+        const {data} = await axios({
+            url: 'http://localhost:3000/checkUser',
+            method: 'post',
+            data: {
+                email
+            }
+        })
+        return data.isValid
+    }
+    catch(err) {
+        console.log(err);
+        throw ({errCode: 'INVALID_ACCOUNT', message: 'Wrong account!'})
+    }
+}
--- server/services/favorites/testing/favorite.test.js
+++ server/services/favorites/testing/favorite.test.js
@@ -1,15 +1,18 @@
 const request = require('supertest')
 const app = require('../app.js')
-const url = 'ini ceritanya url'
+const image_url = 'https://i.stack.imgur.com/NlSfJ.png?s=328&g=1'
+const video_url = 'https://www.youtube.com/watch?v=1AhOK4UwAMs'
+const idIg = '123456'
+const username = 'justin'
 let id = {
     post: '',
     story: '',
     igtv: '',
     highlight: ''
 }
-const access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlY29yaW5lQG1haWwuY29tIiwidXNlcm5hbWUiOiJzaWFwYSIsImlhdCI6MTYwMDc3MzQ4OX0.QAasX5utUdGUfyVdqzZ8TxYCiDUK80fldbned_pF-Ww'
+const access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsInVzZXJuYW1lIjoiZ3JhY2UiLCJlbWFpbCI6ImdyYWNlMUBtYWlsLmNvbSIsImlhdCI6MTYwMDc4OTEwMX0.9-JTIo_H2kO6fwDhTTD-Mk-DRUOzjK45Hz_jOWAMaHE'
 
-describe('GET /favorites successfully', () => {
+describe('GET /favorites/:type successfully', () => {
     it('test get favorite posts from login user will return array of posts', (done)=>  {
         request(app)
         .get(`/favorites/posts`)
@@ -111,13 +114,57 @@ describe('GET /favorites failed', () => {
             done()
         })
     })
+
+    it('test get favorite posts from wrong url user will return message', (done)=>  {
+        request(app)
+        .get(`/favorites/wrong`)
+        .set('access_token', access_token)
+        .set('Accept', 'application/json')
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Data not found')
+            expect(response.status).toBe(404)
+            done()
+        })
+    })
+
+    it('test get favorite posts from wrong url user will return message', (done)=>  {
+        request(app)
+        .get(`/favorites`)
+        .set('access_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Img4QG1haWwuY29tIiwidXNlcm5hbWUiOiJoOCIsImlhdCI6MTYwMDc4NzQyN30.oq-ZryrVAtyRyrJUrW2-FLfhJUUOcs46XMhXjb2Ye9U')
+        .set('Accept', 'application/json')
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Wrong account!')
+            expect(response.status).toBe(401)
+            done()
+        })
+    })
+
+    it('test get favorite with wrong jwt ', (done)=>  {
+        request(app)
+        .get(`/favorites`)
+        .set('access_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsInVzZXJuYW1lIjoiZ3JhY2UiLCJpYXQiOjE2MDA3ODg1MTd9.IcHcyYTKG47Guhsg9Mi7oJlsycvWVSfKNgwiLaKAdvo')
+        .set('Accept', 'application/json')
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Wrong account!')
+            expect(response.status).toBe(401)
+            done()
+        })
+    })
+
 })
 
 describe('POST /favorites successfully', () => {
     it('test post favorite posts from login user will return object of new posts', (done) =>  {
         request(app)
         .post(`/favorites/posts`)
-        .send({url})
+        .send({
+            image_url,
+            idIg,
+            username
+        })
         .set('Accept', 'application/json')
         .set('access_token', access_token)
         .then(response => {
@@ -132,7 +179,11 @@ describe('POST /favorites successfully', () => {
     it('test post favorite stories from login user will return object of stories', (done) =>  {
         request(app)
         .post(`/favorites/stories`)
-        .send({url})
+        .send({
+            image_url,
+            idIg,
+            username
+        })
         .set('Accept', 'application/json')
         .set('access_token', access_token)
         .then(response => {
@@ -147,7 +198,11 @@ describe('POST /favorites successfully', () => {
     it('test post favorite highlight from login user will return object of highlights', (done) =>  {
         request(app)
         .post(`/favorites/highlights`)
-        .send({url})
+        .send({
+            image_url,
+            idIg,
+            username
+        })
         .set('Accept', 'application/json')
         .set('access_token', access_token)
         .then(response => {
@@ -162,7 +217,11 @@ describe('POST /favorites successfully', () => {
     it('test post favorite igtv from login user will return object of igtv', (done) =>  {
         request(app)
         .post(`/favorites/igtvs`)
-        .send({url})
+        .send({
+            image_url,
+            idIg,
+            username
+        })
         .set('Accept', 'application/json')
         .set('access_token', access_token)
         .then(response => {
@@ -175,7 +234,172 @@ describe('POST /favorites successfully', () => {
     })
 })
 
+describe('POST /favorites failed', () => {
+    it('test add wrong type will return object of new posts', (done) =>  {
+        request(app)
+        .post(`/favorites/wrong`)
+        .send({
+            image_url,
+            idIg,
+            username
+        })
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Data not found')
+            expect(response.status).toBe(404)
+            done()
+        })
+    })
 
+
+})
+
+describe('GET /favorites successfully', () => {
+    it('test successfully get all favorites by userLogin email', (done) =>  {
+        request(app)
+        .get(`/favorites`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            expect(response).toHaveProperty('body', expect.any(Object))
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+})
+
+describe('GET /favorites/:id/:type successfully', () => {
+    it('test get favorite post by id from login user will return object selected id', (done) =>  {
+        request(app)
+        .get(`/favorites/${id.post}/posts`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('_id', expect.any(String))
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+
+    it('test get favorite highlight by id from login user will return object selected id', (done) =>  {
+        request(app)
+        .get(`/favorites/${id.highlight}/highlights`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('_id', expect.any(String))
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+
+    it('test get favorite story by id from login user will return object selected id', (done) =>  {
+        request(app)
+        .get(`/favorites/${id.story}/stories`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('_id', expect.any(String))
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+
+    it('test get favorite igtv by id from login user will return object selected id', (done) =>  {
+        request(app)
+        .get(`/favorites/${id.igtv}/igtvs`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('_id', expect.any(String))
+            expect(response.status).toBe(200)
+            done()
+        })
+    })
+
+
+
+})
+
+describe('GET /favorites/:id/:type failed', () => {
+    it('test failed get favorite post by id will return message error', (done) =>  {
+        request(app)
+        .get(`/favorites/${id.highlight}/posts`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Data not found')
+            expect(response.status).toBe(404)
+            done()
+        })
+    })
+
+    it('test failed get favorite post by wrong id will return message error', (done) =>  {
+        request(app)
+        .get(`/favorites/1/posts`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Invalid Id')
+            expect(response.status).toBe(404)
+            done()
+        })
+    })
+
+    it('test failed get favorite post by wrong link will return message error', (done) =>  {
+        request(app)
+        .get(`/favorites/${id.highlight}/wrong`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Data not found')
+            expect(response.status).toBe(404)
+            done()
+        })
+    })
+
+})
+
+describe('DELETE /favorites failed', () => {
+    it('test delete favorite wrong url', (done) =>  {
+        request(app)
+        .delete(`/favorites/${id.post}/highlight`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Data not found')
+            expect(response.status).toBe(404)
+            done()
+        })
+    })
+
+
+
+    it('test delete favorite wrong login', (done) =>  {
+        request(app)
+        .delete(`/favorites/${id.post}/posts`)
+        .set('Accept', 'application/json')
+        .set('access_token', 'testingwrongaccess_token')
+        .then(response => {
+            const { body } = response
+            console.log(body, 'ini body test del');
+            expect(body).toHaveProperty('message', ['Please login first'])
+            expect(response.status).toBe(401)
+            done()
+        })
+    })
+
+})
 
 describe('DELETE /favorites successfully', () => {
     it('test delete favorite posts from login user will return the deleted new post', (done) =>  {
@@ -229,4 +453,18 @@ describe('DELETE /favorites successfully', () => {
             done()
         })
     })
+
+    it('test delete favorite wrong url', (done) =>  {
+        request(app)
+        .delete(`/favorites/${id.post}/posts`)
+        .set('Accept', 'application/json')
+        .set('access_token', access_token)
+        .then(response => {
+            const { body } = response
+            expect(body).toHaveProperty('message', 'Data not found')
+            expect(response.status).toBe(404)
+            done()
+        })
+    })
 })
+
